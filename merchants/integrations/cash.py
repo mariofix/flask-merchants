9c736a6fa77a3416c00e2ab18dc6b0b010fb686f
Mario Hernandez <mariofix@proton.me>
2025-01-20 01:17:29 -0300

    cleanup (#2)
    * cleanup
    * workflow
    * coverage
    * poetry.lock
    * remove cache workflow
    * codacy static analysis
    * codacy security

--- merchants/integrations/cash.py
+++ merchants/integrations/cash.py
@@ -4,5 +4,5 @@ from ..core import CoreProvider
 class CashProvider(CoreProvider):
     name = "cash-provider"
 
-    def create():
+    def create(self):
         return {"url": "url", "transaction": "tr_1234"}
