98a8751cd1e86e2cb229dce67730c79170c9c24b
Mario Hernandez <mariofix@proton.me>
2025-12-01 00:39:58 -0300

    pos venta

--- store/app/pos/routes.py
+++ store/app/pos/routes.py
@@ -6,3 +6,8 @@ pos_bp = Blueprint("pos", __name__)
 @pos_bp.route("/", methods=["GET"])
 def index():
     return render_template("pos/dashboard.html")
+
+
+@pos_bp.route("/venta", methods=["GET"])
+def venta():
+    return render_template("pos/venta.html")
